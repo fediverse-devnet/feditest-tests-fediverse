01dc249f0d064a82d1a86f5b8c5dac84fe0b2b57
Johannes Ernst <git@j12t.org>
2024-04-12 10:55:47 -0700

    Track FediTest Add demo test plan for SaasDriver

--- tests/webfinger/test_server_4_2__4_do_not_accept_malformed_resource_parameters.py
+++ tests/webfinger/test_server_4_2__4_do_not_accept_malformed_resource_parameters.py
@@ -10,15 +10,14 @@ from hamcrest import assert_that, equal_to
 from feditest import step
 from feditest.protocols.webfinger import WebFingerClient, WebFingerServer
 
-# @step
+@step
 def do_not_access_malformed_resource_parameters_not_percent_encoded(
         client: WebFingerClient,
         server: WebFingerServer
 ) -> None:
     # We use the lower-level API from WebClient because we can't make the WebFingerClient do something invalid
-
     test_id : str = server.obtain_account_identifier()
-    hostname : str = server.get_hostname()
+    hostname : str = server.hostname()
 
     malformed_webfinger_uri : str = f"https://{hostname}/.well-known/webfinger?resource={test_id}"
 
@@ -34,7 +33,7 @@ def do_not_access_malformed_resource_parameters_double_equals(
     # We use the lower-level API from WebClient because we can't make the WebFingerClient do something invalid
 
     test_id = server.obtain_account_identifier()
-    hostname : str = server.get_hostname()
+    hostname : str = server.hostname()
 
     malformed_webfinger_uri = f"https://{hostname}/.well-known/webfinger?resource=={urllib.parse.quote(test_id)}"
 
