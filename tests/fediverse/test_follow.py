22f9edb611f3ddf36f817e11634e2db6b68161f4
Johannes Ernst <git@j12t.org>
2024-04-15 20:52:08 -0700

    Merged

--- tests/fediverse/test_follow.py
+++ tests/fediverse/test_follow.py
@@ -19,17 +19,16 @@ def follow(
     leader_actor_uri   = to_be_leader_node.obtain_actor_document_uri()
     follower_actor_uri = to_be_follower_node.obtain_actor_document_uri()
 
-    # FIXME
-    # leader_actor_existing = to_be_leader_node.instantiate_actor_from_actor_uri(leader_actor_uri)
-    # assert_that(leader_actor_existing.followers, is_not(has_item(follower_actor_uri)))
+    leader_actor_existing = to_be_leader_node.instantiate_actor_from_actor_uri(leader_actor_uri)
+    assert_that(leader_actor_existing.followers, is_not(has_item(follower_actor_uri)))
 
-    # follower_actor_existing = to_be_follower_node.instantiate_actor_from_actor_uri(follower_actor_uri)
-    # assert_that(follower_actor_existing.following, is_not(has_item(leader_actor_uri)))
+    follower_actor_existing = to_be_follower_node.instantiate_actor_from_actor_uri(follower_actor_uri)
+    assert_that(follower_actor_existing.following, is_not(has_item(leader_actor_uri)))
 
     to_be_follower_node.make_a_follow_b(follower_actor_uri, leader_actor_uri, to_be_leader_node)
 
-    # leader_actor_new = to_be_leader_node.instantiate_actor_from_actor_uri(leader_actor_uri)
-    # assert_that(leader_actor_new.followers, has_item(follower_actor_uri)))
+    leader_actor_new = to_be_leader_node.instantiate_actor_from_actor_uri(leader_actor_uri)
+    assert_that(leader_actor_new.followers, has_item(follower_actor_uri))
 
-    # follower_actor_new = to_be_follower_node.instantiate_actor_from_actor_uri(follower_actor_uri)
-    # assert_that(follower_actor_new.following, has_item(leader_actor_uri))
+    follower_actor_new = to_be_follower_node.instantiate_actor_from_actor_uri(follower_actor_uri)
+    assert_that(follower_actor_new.following, has_item(leader_actor_uri))
