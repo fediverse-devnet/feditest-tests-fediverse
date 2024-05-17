18c41f7b70955df8f4d409436005008822e9b49d
Steve Bate <steve-bate@users.noreply.github.com>
2024-05-17 18:32:36 +0200

    Reduce the number of rel test cases (#81)

--- tests/webfinger/server/4__1_accepts_all_link_rels_in_query.py
+++ tests/webfinger/server/4__1_accepts_all_link_rels_in_query.py
@@ -1,49 +1,48 @@
-from hamcrest import assert_that, equal_to
-
 from feditest import test
 from feditest.protocols.webfinger import WebFingerClient, WebFingerServer
 from feditest.protocols.webfinger.traffic import WebFingerQueryResponse
 from feditest.protocols.webfinger.utils import link_subset_or_equal_to
 from feditest.reporting import info
+from hamcrest import assert_that, equal_to
 
 # Note: we do not try all the known rel values, only the ones known to be used in a webfinger context
 # See also https://fedidevs.org/reference/webfinger/
 
-KNOWN_RELS = ( # known to be used in webfinger files
+KNOWN_RELS = [ # known to be used in webfinger files
 #    'http://a9.com/-/spec/opensearch/1.1/',
 #    'http://apinamespace.org/atom',
 #    'http://apinamespace.org/twitter',
 #    'http://gmpg.org/xfn/11',
-    'http://joindiaspora.com/guid',
-    'http://microformats.org/profile/hcard',
-    'http://openid.net/specs/connect/1.0/issuer',
-    'http://ostatus.org/schema/1.0/subscribe',
-    'http://purl.org/nomad',
-    'http://purl.org/openwebauth/v1',
-    'http://purl.org/openwebauth/v1#redirect',
-    'http://purl.org/zot/protocol/6.0',
-#    'http://salmon-protocol.org/ns/salmon-mention',
-#    'http://salmon-protocol.org/ns/salmon-replies',
-#    'http://schemas.google.com/g/2010#updates-from',
-#    'http://specs.openid.net/auth/2.0/provider',
-#    'http://webfinger.net/rel/avatar',
-    'http://webfinger.net/rel/blog',
-    'http://webfinger.net/rel/profile-page',
+#     'http://joindiaspora.com/guid',
+#     'http://microformats.org/profile/hcard',
+#     'http://openid.net/specs/connect/1.0/issuer',
+#     'http://ostatus.org/schema/1.0/subscribe',
+#     'http://purl.org/nomad',
+#     'http://purl.org/openwebauth/v1',
+#     'http://purl.org/openwebauth/v1#redirect',
+#     'http://purl.org/zot/protocol/6.0',
+# #    'http://salmon-protocol.org/ns/salmon-mention',
+# #    'http://salmon-protocol.org/ns/salmon-replies',
+# #    'http://schemas.google.com/g/2010#updates-from',
+# #    'http://specs.openid.net/auth/2.0/provider',
+# #    'http://webfinger.net/rel/avatar',
+#     'http://webfinger.net/rel/blog',
+#     'http://webfinger.net/rel/profile-page',
 
-    'describedby',
-    'diaspora-public-key',
-    'magic-public-key',
+#     'describedby',
+#     'diaspora-public-key',
+#     'magic-public-key',
 #    'salmon',
     'self'
-)
+]
 
-UNKNOWN_RELS = ( # not known to be used in webfinger files, likely not real
-    'https://webfinger.example/rel/profile-page',
-#    'https://webfinger.example/rel/businesscard',
-    'http://example.com/',
+UNKNOWN_RELS = [ # not known to be used in webfinger files, likely not real
+#     'https://webfinger.example/rel/profile-page',
+# #    'https://webfinger.example/rel/businesscard',
+#     'http://example.com/',
 
     'something-else'
-)
+]
 
 @test
 def accepts_known_link_rels_in_query(
