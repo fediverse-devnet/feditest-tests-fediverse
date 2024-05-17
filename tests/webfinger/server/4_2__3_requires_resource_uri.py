7c254a2cd495dc482dc5b7227787aa6f067f98d6
Steve Bate <svc-github@stevebate.net>
2024-05-17 13:08:25 +0200

    Don't fail for missing content-type in non-JRD assertions

--- tests/webfinger/server/4_2__3_requires_resource_uri.py
+++ tests/webfinger/server/4_2__3_requires_resource_uri.py
@@ -1,9 +1,18 @@
-from hamcrest import any_of, assert_that, calling, equal_to, greater_than_or_equal_to, is_not, starts_with, raises
-
 from feditest import test
 from feditest.protocols.web.traffic import HttpResponse
 from feditest.protocols.webfinger import WebFingerClient, WebFingerServer
 from feditest.protocols.webfinger.traffic import ClaimedJrd
+from hamcrest import (
+    any_of,
+    assert_that,
+    calling,
+    equal_to,
+    greater_than_or_equal_to,
+    is_not,
+    raises,
+    starts_with,
+)
+
 
 def interpret_payload_as_jrd(payload: str):
     """
@@ -27,10 +36,11 @@ def requires_resource_uri(
 
     response_without : HttpResponse = client.http_get(uri_without).response
     assert_that(response_without.http_status, equal_to(400))
-    assert_that(response_without.response_headers['content-type'],
+    content_type = response_without.response_headers.get('content-type', "")
+    assert_that(content_type,
         is_not(any_of(
                 equal_to('application/jrd+json'),
                 starts_with('application/jrd+json;'))))
-    if "application/json" in response_without.response_headers.get('content-type', ""):
+    if "application/json" in content_type:
         assert_that(calling( lambda: interpret_payload_as_jrd(response_without.payload)), raises(RuntimeError))
 
