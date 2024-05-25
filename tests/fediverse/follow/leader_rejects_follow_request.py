f3a196d1bcc39bf00fb662c955fa220e529fa30a
Johannes Ernst <git@j12t.org>
2024-05-24 20:32:32 -0700

    Fix super() calls. Closes #78.

--- tests/fediverse/follow/leader_rejects_follow_request.py
+++ tests/fediverse/follow/leader_rejects_follow_request.py
@@ -22,18 +22,18 @@ class RejectsFollowTest(AbstractFollowTest):
 
     @step
     def get_actors(self):
-        super.get_actors()
+        super().get_actors()
 
 
     @step
     def get_collections(self):
-        super.get_collections();
+        super().get_collections();
 
 
     @step
     def initially_unrelated(self):
         # Initially they are unrelated
-        super.test_unrelated();
+        super().test_unrelated();
 
 
     @step
@@ -59,4 +59,4 @@ class RejectsFollowTest(AbstractFollowTest):
     @step
     def still_unrelated(self):
         # Initially they are unrelated
-        super.test_unrelated();
+        super().test_unrelated();
