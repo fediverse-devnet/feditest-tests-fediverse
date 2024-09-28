81a1ff47d0b5a8243d032614e971415c6b2f0b54
Johannes Ernst <jernst@users.noreply.github.com>
2024-09-28 10:51:39 -0700

    Closes #147 (#156)
    * Closes #147
    
    * Remove separate mention file, already addessed per #152
    
    * Remove obsolete comment
    
    * Remove temporary testplan file that wasn't supposed to be checked in.
    
    ---------
    
    Co-authored-by: Johannes Ernst <git@j12t.org>

--- tests/fediverse/for0.4/reply.py
+++
@@ -1,7 +0,0 @@
-# https://github.com/fediverse-devnet/feditest-tests-fediverse/issues/148
-#
-# Two nodes
-# Actor A creates Note X addressed to Actor B
-# B receives Note
-# B creates Note in Y in response to X
-# A receives Note
