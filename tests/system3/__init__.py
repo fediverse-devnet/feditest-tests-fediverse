0da1a43df3280be6f532bc534b075691b3cfce37
Johannes Ernst <git@j12t.org>
2024-10-21 16:22:41 -0700

    Clean up directory hierarchy, add comments

---
+++ tests/system3/__init__.py
@@ -0,0 +1,6 @@
+"""
+A set of tests testing Fediverse system-level behaviors, involving three Nodes.
+
+Many of these common behaviors are agreed to and implemented across the Fediverse
+even if they are not defined as-such in a standard.
+"""
